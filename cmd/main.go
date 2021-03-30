63c9d01228d7082b9f732ebbf74792f80f327ce4
zuodengpeng <cqzuodp@163.com>
2021-03-30 08:04:29 +0800

    fix: add csi driver.

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"ez-cloud/hostpath-provisioner/pkg/hostfsCsi"
 	"ez-cloud/hostpath-provisioner/pkg/pvc"
 	"flag"
 	"k8s.io/klog/v2"
 )
 
-var hostDir = flag.String("host-dir", pvc.DefaultMountDir, "localpv host dirs")
+const (
+	CSI_NAME = "csi-hostfs"
+	VERSION  = "1.0.0"
+)
+
+var nodeid = flag.String("nodeid", "", "node id")
+var endpoint = flag.String("endpoint", pvc.CsiEndpoint, "csi socket unix path")
+var hostfsType = flag.String("hostfs-type", "", "ns,ids,cs")
+
+var hostDir = flag.String("host-dir", pvc.DefaultMountDir, "host dirs")
 var provisionerName = flag.String("provisioner-name", pvc.ProvisionerName, "external provisioner name ")
 
 func main() {
 	klog.InitFlags(nil)
 	flag.Parse()
-	pvc.StartHotFsProvisioner(*provisionerName, *hostDir)
+	csiDriver := hostfsCsi.NewHostfsCsiDriver(CSI_NAME, VERSION, *nodeid, *endpoint)
+	csiDriver.Start(*hostfsType)
 }
--- pkg/pvc/constats.go
+++ pkg/pvc/constats.go
@@ -3,6 +3,7 @@ package pvc
 const (
 	ProvisionerName   = "ez-cloud/hostpath"
 	DefaultMountDir   = "/works"
+	CsiEndpoint       = "unix:///csi/csi.sock"
 	ProvisionerId     = "CloudhostpathProvisionerId"
 	ProvisionerNodeId = "CloudhostpathNodeId"
 )
